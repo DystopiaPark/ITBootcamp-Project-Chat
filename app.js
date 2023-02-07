78e2390db0b33f5ba19f0e324afaf7d3d23f8b5a
Filip <68464364+DystopiaPark@users.noreply.github.com>
2023-02-07 02:14:18 +0100

    more text

--- app.js
+++ app.js
@@ -140,7 +140,7 @@ chatSection.addEventListener("click", (e) => {
       li.remove();
       if (
         confirm(
-          "Do you also want to permanently delete the message from another user?"
+          "Do you also want to also permanently delete the message from another user?"
         ) == true
       ) {
         chatroom.chats
